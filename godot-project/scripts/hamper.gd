f1fe0b32c40c312a535fe4c970f7663606e1a31d
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-06-20 19:00:39 -0700

    can drag hamper

--- godot-project/scripts/hamper.gd
+++ godot-project/scripts/hamper.gd
@@ -1,12 +1,17 @@
 extends RigidBody2D
 
+@export var drag_force : float = 500.0
+@export var drag_damping : float = 50.0
+
 var dragging : bool
 
 func _ready() -> void:
 	input_event.connect(_input_event)
 
-func _process(_delta: float) -> void:
-	pass
+func _physics_process(_delta: float) -> void:
+	if dragging:
+		var to_mouse := get_global_mouse_position() - global_position
+		apply_central_force(to_mouse * drag_force - linear_velocity * drag_damping)
 
 func _input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
